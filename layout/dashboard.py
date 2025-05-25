e6fb7c2ecc23d5c24e834f5147430ea12f325a38
kuranez <114447397+kuranez@users.noreply.github.com>
2025-05-25 20:48:44 +0200

    Delete layout directory

--- layout/dashboard.py
+++
@@ -1,6 +0,0 @@
-import panel as pn
-
-def build_layout(map_plot, bar_by_year, bar_by_country, year_slider, country_select):
-    tabs = pn.Tabs(...)
-    layout = pn.Column(pn.pane.Plotly(map_plot), tabs)
-    return pn.template.FastListTemplate(..., main=[layout])
